175f1d1b6fd9b21c8d42b07470644a013cc8a6d1
Stone Fang <nanoix9@gmail.com>
2019-09-16 15:01:34 +1200

    fix

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -11,16 +11,16 @@ var app = express();
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
   console.info('under production environment')
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 }
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
