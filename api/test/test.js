fd19e1c1cd0889d9e50612e3924fb1de00df5d15
Stone Fang <nanoix9@gmail.com>
2019-09-21 02:15:20 +1200

    add test case

---
+++ api/test/test.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+
+describe('Placeholder', function() {
+  describe('#placeholder()', function() {
+    it('should be true', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
+
