7ff66dd6d74cc8551fb74e91da83c08e7c8c12ca
Oleg Kazantsev <ok020373@gmail.com>
2019-09-25 00:08:55 +1200

    Getting the initial user list has been implemented

---
+++ api/model/users.js
@@ -0,0 +1,65 @@
+exports.getInitialUserList = function() {
+    // Eirik Guestsson - Guest
+    var guest = {
+        user_id: 'guest',
+        user_password: 'Passw0rd',
+        user_full_name: 'Eirik Guestsson',
+        user_email: 'eirik.guestsson@allzeek.com',
+        user_role: 'guest'
+    };
+
+    // Veleif Usersson - User
+    var user = {
+        user_id: 'user',
+        user_password: 'Passw0rd',
+        user_full_name: 'Veleif Usersson',
+        user_email: 'veleif.usersson@allzeek.com',
+        user_role: 'user'
+    };
+
+    // Torhild Analystdottir - Analyst
+    var analyst = {
+        user_id: 'analyst',
+        user_password: 'Passw0rd',
+        user_full_name: 'Torhild Analystdottir',
+        user_email: 'torhild.analystdottir@allzeek.com',
+        user_role: 'analyst'
+    };
+
+    // Ragneid Moderatordottir - Moderator
+    var moderator = {
+        user_id: 'moderator',
+        user_password: 'Passw0rd',
+        user_full_name: 'Ragneid Moderatordottir',
+        user_email: 'ragneid.moderatordottir@allzeek.com',
+        user_role: 'moderator'
+    };
+
+    // Ornolf Submittersson - Submitter
+    var submitter = {
+        user_id: 'submitter',
+        user_password: 'Passw0rd',
+        user_full_name: 'Ornolf Submittersson',
+        user_email: 'ornolf.submittersson@allzeek.com',
+        user_role: 'submitter'
+    };
+
+    // Asgard Admindottir - Admin
+    var admin = {
+        user_id: 'admin',
+        user_password: 'Passw0rd',
+        user_full_name: 'Asgard Admindottir',
+        user_email: 'asgard.admindottir@allzeek.com',
+        user_role: 'admin'
+    };
+
+    var userList = [];
+    userList.push(guest);
+    userList.push(user);
+    userList.push(analyst);
+    userList.push(moderator);
+    userList.push(submitter);
+    userList.push(admin);
+
+    return userList;
+}
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const sysusers = require('../model/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.info('users');
+ 
+  var userList = sysusers.getInitialUserList();
+  res.json(userList);
 });
 
 module.exports = router;
