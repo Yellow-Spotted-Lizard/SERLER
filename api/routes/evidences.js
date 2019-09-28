3af9ee21e681af5c837d940181297428c8d946f4
Stone Fang <nanoix9@gmail.com>
2019-09-28 19:16:07 +1200

    implement simple search api

--- api/routes/evidences.js
+++ api/routes/evidences.js
@@ -54,4 +54,22 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/search', function(req, res, next) {
+  let title_contains = req.query.title_contains;
+  let limit = req.query.limit || 20;
+
+  Evidence
+  .find({"title": { "$regex": title_contains, "$options": "i" }})
+  .limit(limit)
+  .exec()
+  .then(evs => {
+    debug(`search evidences query ${JSON.stringify(req.query)} and return: ${evs}`);
+    res.json(evs);
+  })
+  .catch(err => {
+    debug(`error in find evidence for query: ${JSON.stringify(req.query)}, error: ${err}`);
+    res.status(500).send('an error occurred: ' + err);
+  });
+});
+
 module.exports = router;
