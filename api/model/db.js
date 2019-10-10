1d084f7e8c66dbd4b351f7a2d2bf19a9b9552a65
Oleg Kazantsev <ok020373@gmail.com>
2019-10-10 13:27:25 +1300

    Unit tests of services have been partially impl

--- api/model/db.js
+++ api/model/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 var debug = require('debug')('api:mongodb');
 
-const db_name = process.env.MONGODB_DB_NAME;
-const url = process.env.MONGODB_URI;
+const db_name = process.env.MONGODB_DB_NAME || 'serler_debug';
+const url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
 
 mongoose
-  .connect(url, {dbName: db_name})
+  //.connect(url, {dbName: db_name})
+  .connect(url + '/' + db_name,  { useNewUrlParser: true }) 
   .catch(err => {
     console.error(`cannot connect to mongodb: ${url}, db name: ${db_name}`);
   });
---
+++ api/test/admin-service-test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+const userService = require('../services/admin-service');
+
+// Test of seedUsers
+
+describe('adminService', function() {
+  describe('#seedUsers()', function() {
+    it('should be true', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
+
+// Test of disposeUsers
+
+describe('adminService', function() {
+  describe('#disposeUsers()', function() {
+    it('should be true', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
