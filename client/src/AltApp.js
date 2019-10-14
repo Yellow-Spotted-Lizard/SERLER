c227c3c10a85bff929d5499fd84057a53180e5d8
Oleg Kazantsev <ok020373@gmail.com>
2019-10-14 13:04:14 +1300

    Revert "Debug search has been removed"
    This reverts commit 0530b81abca456b8d714b35e37279eb41ee30c7d.

--- client/src/AltApp.js
+++ client/src/AltApp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { AuthProviderImpl } from "./auth/auth-provider-impl";
-//import Search from "./components/search.component";
+import Search from "./components/search.component";
 import AltSearch from "./components/alt-search.component";
 import Admin from "./components/admin.component";
 import About from "./components/about.component";
@@ -25,7 +25,10 @@ class AltApp extends Component {
                                 <ul className="navbar-nav mr-auto">
                                     <li className="navbar-item">
                                         <Link to="/search" className="nav-link">Search</Link>
-                                    </li>                                 
+                                    </li>
+                                    <li className="navbar-item">
+                                        <Link to="/debug-search" className="nav-link">Debug search</Link>
+                                    </li>                                    
                                     <li className="navbar-item">
                                         <Link to="/admin" className="nav-link">Admin</Link>
                                     </li>
@@ -38,6 +41,7 @@ class AltApp extends Component {
                         <br />
                         <Route path="/" exact component={AltSearch} />
                         <Route path="/search" component={AltSearch} />
+                        <Route path="/debug-search" component={Search} />
                         <Route path="/admin" component={Admin} />
                         <Route path="/about" component={About} />
                     </div>
