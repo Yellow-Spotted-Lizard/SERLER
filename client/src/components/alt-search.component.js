e0eee279baffd09a91027f7e54c1c0ca4def4867
Oleg Kazantsev <ok020373@gmail.com>
2019-10-21 15:40:22 +1300

    Getting values for search has been implemented

--- client/src/components/alt-search.component.js
+++ client/src/components/alt-search.component.js
@@ -68,35 +68,91 @@ export default class About extends Component {
         
         // -- Title
         var title = "";
-        var tileMode = "";
+        var tileMode = "contains";
 
         let queryTextData = localStorage.getItem("serler_queryText");
         if (queryTextData != null) {
             title = JSON.parse(queryTextData);
         }
-        console.log('title = ' + title);
 
         let conditionData = localStorage.getItem("serler_condition");
         if (conditionData != null) {
             tileMode = JSON.parse(conditionData);
         }
+
+        // DEBUG!!!
+        console.log('title = ' + title);
         console.log('tileMode = ' + tileMode);
 
         // -- Calendar
-        //var beginYear = "";
-        //var endYear = "";
-        //var yearMode = "";
+        var beginYear = "";
+        var endYear = "";
+        var yearMode = "all";
+
+        let fromValueData = localStorage.getItem("serler_from_year");
+        if (fromValueData != null) {
+            beginYear = JSON.parse(fromValueData);
+        }
+
+        let toValueData = localStorage.getItem("serler_to_year");
+        if (toValueData != null) {
+            endYear = JSON.parse(toValueData);
+        }
+
+        let rangeModeData = localStorage.getItem("serler_range_mode");
+        if (rangeModeData != null) {
+            yearMode = JSON.parse(rangeModeData);
+        }
+
+        // DEBUG!!!
+        console.log('beginYear = ' + beginYear);
+        console.log('endYear = ' + endYear);
+        console.log('yearMode = ' + yearMode);
 
         // -- Filters
-        var filters = [];
         var filterCount = 0;
+        let filterCountData = localStorage.getItem("serler_filter_count");        
+        if (filterCountData != null) {
+            filterCount = JSON.parse(filterCountData);
+        }
+
+        var filters = [];
         for (var i = 0; i < filterCount; i++) {
+            var field= "method";
+            var operator = "contains";
+            var value = "tdd";
+
+            let filedData = localStorage.getItem("serler_filter_id_" + i + "_field");
+            if (filedData != null) {
+                field = JSON.parse(filedData);
+
+            }
+
+            let operatorData = localStorage.getItem("serler_filter_id_" + i +  "_operator");
+            if (operatorData != null) {
+                operator = JSON.parse(operatorData);
+            }
+
+            let valueData = localStorage.getItem("serler_filter_id_" + i + "_value");
+            if (valueData != null) {
+                value = JSON.parse(valueData);
+            }
+
             var filter = {
-                fieled: "",
-                operator: "",
-                value: "",
+                fieled: field,
+                operator: operator,
+                value: value,
             }
             filters.push(filter);
+        }
+
+        // DEBUG!!!
+        console.log('filterCount = ' + filterCount);
+        for (i = 0; i < filterCount; i++)
+        {
+            console.log('ffilters[' + i + '].fieled = ' + filters[i].fieled);
+            console.log('ffilters[' + i + '].operator = ' + filters[i].operator);
+            console.log('ffilters[' + i + '].value = ' + filters[i].value);
         }
 
         // Prepare query
